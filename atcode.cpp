0b172fd9efb699dc0a6bf0330a7893e8ab9b8cfc
BingLingYun <2210797528@qq.com>
2024-05-31 16:16:13 +0800

    Add files via upload
    单组数据最初模板

---
+++ atcode.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+#define i64 long long
+#define pii pair<int,int>
+#define pll pair<i64,i64>
+#define int128 __int128_t
+#define ull unsigned long long
+using namespace std;
+
+const i64 mod=1e9+7;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    cout<<fixed<<setprecision(10);
+    
+    
+    return 0;
+}
